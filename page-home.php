b3356f4f2d71d7051abbe8e256b398ec76ea9190
Lyle Palagar <lylerpalagar@gmail.com>
2013-09-05 19:41:37 -0600

    added instagram link and changed span size to 3 instead of 4

--- page-home.php
+++ page-home.php
@@ -57,14 +57,17 @@
     </div>
     <div class="span2 pull-right visible-desktop">
       <div class="row-fluid social-pics">
-        <div class="span4">
+        <div class="span3">
           <a href="https://www.facebook.com/icomm.student.media?fref=ts"><img src="<?php bloginfo( 'template_url' ); ?>/img/f_logo.png"></a>
         </div>
-        <div class="span4">
-          <a href="https://twitter.com/byuicomm"><img src="<?php bloginfo( 'template_url' ); ?>/img/twitter_logo.png"></a>
+        <div class="span3">
+          <a href="https://twitter.com/byuiscroll"><img src="<?php bloginfo( 'template_url' ); ?>/img/twitter_logo.png"></a>
         </div>
-        <div class="span4">
+        <div class="span3">
           <a href="http://pinterest.com/byuicomm/"><img src="<?php bloginfo( 'template_url' ); ?>/img/pinterest_logo.png"></a>
+        </div>
+        <div class="span3">
+          <a href="http://instagram.com/byuiscroll/"><img src="<?php bloginfo( 'template_url' ); ?>/img/instagram_icon_large.png"></a>
         </div>
       </div>
     </div>

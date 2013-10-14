754ee8f1cdb7239dcc5be168aedd60ade45ea29e
gbentim <guilherme.guizmo@gmail.com>
2013-10-14 13:15:16 -0600

    more modularization with the social network links

--- ldp.php
+++ ldp.php
@@ -16,22 +16,11 @@
     <div class="span10">
       <h2 class="feature-lead pull-left">Latter-Day Profiles</h2>
     </div>
-    <div class="span2 pull-right visible-desktop">
-      <div class="row-fluid social-pics">
-        <div class="span3">
-          <a href="https://www.facebook.com/icomm.student.media?fref=ts"><img src="<?php bloginfo( 'template_url' ); ?>/img/f_logo.png"></a>
-        </div>
-        <div class="span3">
-          <a href="https://twitter.com/byuicomm"><img src="<?php bloginfo( 'template_url' ); ?>/img/twitter_logo.png"></a>
-        </div>
-        <div class="span3">
-          <a href="http://pinterest.com/byuicomm/"><img src="<?php bloginfo( 'template_url' ); ?>/img/pinterest_logo.png"></a>
-        </div>
-         <div class="span3">
-          <a href="http://instagram.com/byuiscroll/"><img src="<?php bloginfo( 'template_url' ); ?>/img/instagram_icon_large.png"></a>
-        </div>
-      </div>
-    </div>
+
+    <!-- This file contains the social media icons for the desktop version and also 
+      the mobile version of the website -->
+      <?php include_once "social-menu.php"; ?>
+      
   </div>
   <hr>
   <div class="row-fluid">

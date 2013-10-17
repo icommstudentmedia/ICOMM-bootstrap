f38368afc6886debf95b4ad04df9aa76ec0f0031
gbentim <guilherme.guizmo@gmail.com>
2013-10-17 01:51:18 -0600

    terms of Use and Privacy Policy

--- footer.php
+++ footer.php
@@ -20,7 +20,8 @@
 		  		</div>
 		  		<div class="span6 pull-right align-right">
 		  			<p>
-		  				<a href = #>Terms of Use</a> | <a href = #>Privacy Policy</a>
+		  				<a href = "/wp-content/themes/icomm-bootstrap/terms-use">Terms of Use</a> | 
+		  				<a href = "/wp-content/themes/icomm-bootstrap/privacy-policy">Privacy Policy</a>
 		  			</p>
 			</div>
 		</div>
---
+++ terms-use.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* This has to go on it to be a template page
+* Template Name: Terms of Use
+*
+* @author Guilherme Bentim <guilherme.guizmo@gmail.com>
+**/	
+get_header();
+?>
+
+    <!-- This file contains the social media icons for the desktop version and also 
+      the mobile version of the website -->
+      <?php include_once "social-menu.php"; ?>
+      
+<div class="container container-narrow">
+	<div class="row-fluid vert-padding">
+		<!-- todo -->
+		<div class="span10">
+			<!-- change and style -->
+			<h2 class="feature"> Terms of Use </h2>
+		</div> 						  
+	</div>
+</div>
+
+	<hr/>
+
+	<div class="row-fluid responsive-about">
+		<div class="span3">	<!-- Left Sidebar-->
+			<figure>
+		<img src="/wp-content/themes/icomm-bootstrap/img/icomm.png" width="120" height="164"/>
+			</figure>
+		</div>				<!-- End Left Sidebar -->
+
+		<div id="post-content" class="span5"> 	<!-- Content -->
+			
+			<h3> Sub Header</h3>
+			<p>Content
+			</p>
+						
+			<h3> Sub Header </h3>
+			<p> Content
+			<p/>
+		</div>						<!-- End Content -->
+	</div>
+
+	<div class="row-fluid"></div> <!-- I added this div so that I can push the footer on the bottom (LP) -->
+
+
+<?php get_footer();?>

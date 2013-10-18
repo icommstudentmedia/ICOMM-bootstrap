45d0f8bc759560d4868dc91147c33b9b3c2bcc0f
gbentim <guilherme.guizmo@gmail.com>
2013-10-18 01:15:36 -0600

    page-legal template added

---
+++ page-legal.php
@@ -0,0 +1,47 @@
+<?php
+   /**
+    *   Template Name: Legal Page
+    *   Date: October 16,2013
+    *   @author Guilherme Bentim, Isaac Andrade
+    **/	
+get_header();
+?>
+
+    <!-- This file contains the social media icons for the desktop version and also 
+      the mobile version of the website -->
+      <?php include_once "social-menu.php"; ?>
+      
+<div class="container container-narrow">
+	<div class="row-fluid vert-padding">
+		<!-- todo -->
+		<div class="span10">
+			<!-- change and style -->
+			<h2 class="feature"> <?php wp_title("", true); ?></h2>
+		</div> 						  
+	</div>
+</div>
+
+	<hr/>
+
+	        <aside class="span3">
+          
+        </aside>
+
+	<div class="row-fluid responsive-about" >
+		<div id="post-content" class="span6">
+					
+			<?php 
+				if(have_posts() ) : while (have_posts() ) : the_post();
+				the_content();
+				endwhile; endif;
+			?>
+			
+		</div>
+	</div>
+
+
+
+	<div class="row-fluid"></div> <!-- I added this div so that I can push the footer on the bottom (LP) -->
+
+
+<?php get_footer();?>

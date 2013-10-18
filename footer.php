f9eebac2184562b8a69dfa830c084210287f9692
zhongyue <pandabryce@gmail.com>
2013-10-17 23:33:38 -0600

    fixed fraking login

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 				<div class="span6 mobile-center">
 		  			<p>&copy; 2013 Scroll / I~Comm Student Media, All Rights Reserved.</p>
 		  		</div>
-		  		<div class="span6 mobile-center pull-right mobile-pull-right align-right">
+		  		<div class="span6 mobile-center desktop-pull-right mobile-pull-right align-right">
 		  			<p>
 		  				<a href = "<?php echo content_url(); ?>/terms-of-use">Terms of Use</a> | 
 		  				<a href = "<?php echo content_url(); ?>/privacy-policy">Privacy Policy</a>

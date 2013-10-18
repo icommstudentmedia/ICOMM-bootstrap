66a5adb65da4d5fdbf2201fdf5847c8524bda45f
zhongyue <pandabryce@gmail.com>
2013-10-17 23:13:06 -0600

    changed the fraking footer

--- footer.php
+++ footer.php
@@ -15,10 +15,10 @@
 	<div class="container footer">
 		<div class="container container-narrow">
 			<div class="row-fluid">
-				<div class="span6 pull-left">
+				<div class="span6 mobile-center">
 		  			<p>&copy; 2013 Scroll / I~Comm Student Media, All Rights Reserved.</p>
 		  		</div>
-		  		<div class="span6 pull-right align-right">
+		  		<div class="span6 mobile-center pull-right mobile-pull-right align-right">
 		  			<p>
 		  				<a href = "../terms-of-use">Terms of Use</a> | 
 		  				<a href = "../privacy-policy">Privacy Policy</a>

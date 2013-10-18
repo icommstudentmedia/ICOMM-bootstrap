cc4ad365f555bea65b79cdb732175e50e3a242fb
zhongyue <pandabryce@gmail.com>
2013-10-17 20:56:55 -0600

    social icon fix

--- social-menu.php
+++ social-menu.php
@@ -8,18 +8,18 @@
 * Hidden on the mobile version
 */
 ?>
-<div class="span2 pull-right visible-desktop">
+<div class="span2 pull-right visible-desktop social-block">
 	<div class="row-fluid social-pics">
-		<div class="span3">
+		<div class="span3 social-icons">
 			<a href="https://www.facebook.com/icomm.student.media?fref=ts"><img src="<?php bloginfo( 'template_url' ); ?>/img/f_logo.png"></a>
 		</div>
-		<div class="span3">
+		<div class="span3 social-icons">
 			<a href="https://twitter.com/byuiscroll"><img src="<?php bloginfo( 'template_url' ); ?>/img/twitter_logo.png"></a>
 		</div>
-		<div class="span3">
+		<div class="span3 social-icons">
 			<a href="http://pinterest.com/byuicomm/"><img src="<?php bloginfo( 'template_url' ); ?>/img/pinterest_logo.png"></a>
 		</div>
-		<div class="span3">
+		<div class="span3 social-icons">
 			<a href="http://instagram.com/byuiscroll/"><img src="<?php bloginfo( 'template_url' ); ?>/img/instagram_icon_large.png"></a>
 		</div>
 	</div>

f5f289b989733d092cc6dc9ae8d985d96b6a5102
Isaac Andrade <isaac.nic@gmail.com>
2013-11-14 01:06:47 -0700

    Initial code for the Scroll Reader (iOS app) Server API

---
+++ includes/scroll-reader/app-controller.php
@@ -0,0 +1,53 @@
+<?php
+/**
+*	Controller for the Scroll Reader app (iOS)
+*	@author Isaac Andrade - isaac.nic@gmail.com
+*	@version 1.0
+*/
+
+// Include the file that has the include for the WP_Query class.
+include ('../../../../../wp-blog-header.php');
+
+// The path to the application handlers (functions)
+$path = 'app-handlers.php';
+
+// Conditionally include the model
+if(file_exists($path))
+{
+	include_once ($path);
+} 
+else
+{
+	// Return a message to client
+}
+
+// Check if 'command' and 'data' came from POST message
+if($_POST['command'])
+{
+	$command = $_POST['command'];
+}
+
+if($_POST['data'])
+{
+	$data = $_POST['data'];	
+}
+
+// Define array of commands
+$handlers = array(
+	'get_articles_home' => 'get_articles_home',
+	'get_articles_by_category' => 'get_articles_by_category',
+	'get_article' => 'get_article',
+	'get_author' => 'get_author',
+	'get_articles_by_author' => 'get_articles_by_author'
+	);
+
+// Debugging Code
+$command = 'get_articles_home';
+$data = '';
+print_r($handlers);
+echo $command . " " . $data;
+
+// Use reflection to call handler functions depending on the values of $command $data
+$handlers[$command]($data);
+
+?>
---
+++ includes/scroll-reader/app-handlers.php
@@ -0,0 +1,44 @@
+<?php
+
+$app_query = new WP_Query();
+
+// username scroll_reader_13
+// password ScrollReaderUser_2013
+
+
+function get_articles_home($data)
+{
+	$args = array(
+		'post_type' => 'post'
+		);
+	
+	$app_query = get_query($args);
+
+	print_r($app_query);
+}
+
+function get_articles_by_category($data)
+{
+	
+}
+
+function get_article($data)
+{
+	
+}
+
+function get_author($data)
+{
+	
+}
+
+function get_articles_by_author($data)
+{
+	
+}
+
+function get_query($args)
+{
+	return new WP_Query($args);
+}
+?>

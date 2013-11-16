751e33c2366a6216eb70094dad7b4eedfb4d87c5
Isaac Andrade <isaac.nic@gmail.com>
2013-11-15 22:35:50 -0700

    Rename image names to fit pattern rule for ad naming

--- includes/scroll-reader/app-controller.php
+++ includes/scroll-reader/app-controller.php
@@ -4,13 +4,10 @@
 *	@author Isaac Andrade - isaac.nic@gmail.com
 *	@version 1.0
 */
-
 // Include the file that has the include for the WP_Query class.
 include ('../../../../../wp-blog-header.php');
-
 // The path to the application handlers (functions)
 $path = 'app-handlers.php';
-
 // Conditionally include the model
 if(file_exists($path))
 {
@@ -20,18 +17,15 @@ else
 {
 	// Return a message to client
 }
-
 // Check if 'command' and 'data' came from POST message
 if($_POST['command'])
 {
 	$command = $_POST['command'];
 }
-
 if($_POST['data'])
 {
 	$data = $_POST['data'];	
 }
-
 // Define array of commands
 $handlers = array(
 	'get_articles_home' => 'get_articles_home',
@@ -40,14 +34,11 @@ $handlers = array(
 	'get_author' => 'get_author',
 	'get_articles_by_author' => 'get_articles_by_author'
 	);
-
 // Debugging Code
 $command = 'get_articles_home';
 $data = '';
 print_r($handlers);
 echo $command . " " . $data;
-
 // Use reflection to call handler functions depending on the values of $command $data
 $handlers[$command]($data);
-
 ?>
--- includes/scroll-reader/app-handlers.php
+++ includes/scroll-reader/app-handlers.php
@@ -1,41 +1,28 @@
 <?php
-
 // !!!! --- If these credentials are not needed, delete the user in the database!!
 // username scroll_reader_13
 // password ScrollReaderUser_2013
-
-
 function get_articles_home($data)
 {
 	$args = array(
 		'post_type' => 'post'
 		);
-	
 	$app_query = get_query($args);
-
+	// Debug
 	print_r($app_query);
 }
-
 function get_articles_by_category($data)
-{
-	
+{	
 }
-
 function get_article($data)
-{
-	
+{	
 }
-
 function get_author($data)
-{
-	
+{	
 }
-
 function get_articles_by_author($data)
-{
-	
+{	
 }
-
 function get_query($args)
 {
 	return new WP_Query($args);
